6c95b48f470e5fed3b61a63e333608d313ff170e
piotr <pbialas7@gmail.com>
2021-12-01 13:00:19 +0100

    Fixed a bug in Material.cpp
    shader_ static field was not initialized.

--- src/Engine/Material.cpp
+++ src/Engine/Material.cpp
@@ -9,6 +9,7 @@
 namespace xe {
 
     GLuint ColorMaterial::color_uniform_buffer_ = 0u;
+    GLuint ColorMaterial::shader_ = 0u;
 
     void ColorMaterial::bind() {
         glBindBufferBase(GL_UNIFORM_BUFFER, 0, color_uniform_buffer_);
