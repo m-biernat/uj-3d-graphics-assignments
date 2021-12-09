98f992c139d4e9453475077c2eaf7fc34239d7ea
Michał Biernat <44023124+m-bt@users.noreply.github.com>
2021-12-09 12:45:17 +0100

    Fixed a minor issue in assignment Textures

--- src/Assignments/Textures/app.cpp
+++ src/Assignments/Textures/app.cpp
@@ -92,20 +92,20 @@ void SimpleShapeApplication::init()
     auto img = stbi_load(texture_file.c_str(), &width, &height, &channels, 0);
     if (!img) {
         spdlog::warn("Could not read image from file `{}'", texture_file);
+    } else {
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        texture_mat->set_texture(texture);
+        texture_mat->set_texture_unit(0);
     }
-
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    texture_mat->set_texture(texture);
-    texture_mat->set_texture_unit(0);
 
 
     glGenBuffers(1, &u_pvm_buffer_);
